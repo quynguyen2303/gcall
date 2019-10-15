d3c199a932e564ffd282038820710a82b4f24fa4
quynguyen2303 <nguyenphuquy2303@gmail.com>
2019-10-15 09:36:12 +0700

    add create contact screen

---
+++ lib/screens/CreateContactScreen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class CreateContactScreen extends StatefulWidget {
+  static const routeName = './create_contact';
+  @override
+  _CreateContactScreenState createState() => _CreateContactScreenState();
+}
+
+class _CreateContactScreenState extends State<CreateContactScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
